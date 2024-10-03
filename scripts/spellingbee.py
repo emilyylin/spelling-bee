a42e57970f7309652b98f92dcc722425913d3077
Emily <s265lin@uwaterloo.ca>
2024-10-03 15:15:54 -0400

    include center letter

--- scripts/spellingbee.py
+++ scripts/spellingbee.py
@@ -2,13 +2,17 @@ import sys
 import itertools
 import bisect
 
-def valid_combinations(words_list, letters):
+#usage: spellingbee.py [letters] [center letter]
+
+def valid_combinations(words_list, letters, center):
     valid_words=set()
     
     for i in range(1, len(letters)+1):
         for j in itertools.product(letters, repeat=i):
             #create combination of word
             word=''.join(j)
+
+            if(word.find(center)==-1): continue
 
             n=bisect.bisect_left(words_list, word)
             if n<len(words_list) and words_list[n]==word:
@@ -21,8 +25,9 @@ words_list = (words_file.read()).split("\n")
 output = open("../assets/output.txt", "w")
 
 letters = sys.argv[1]
+center = sys.argv[2]
 
-valid_words=valid_combinations(words_list, letters)
+valid_words=valid_combinations(words_list, letters, center)
 for word in valid_words:
     output.write(word+"\n")
 
